f362322897e5e442fb68f0cb1a8e1bdc9579adf2
Jessy <berthaultjessy@gmail.com>
2020-12-02 11:50:49 +0100

    comit

--- Cours JS/TP/exo.js
+++ Cours JS/TP/exo.js
@@ -1,6 +1,8 @@
 function exo1() {
 
-    var nb = prompt("Saisir chiffre");
+    alert(isEven(2))
+
+    /*var nb = prompt("Saisir chiffre");
 
     if (nb % 2 == 0) {
 
@@ -9,39 +11,80 @@ function exo1() {
     } else {
 
         alert("Impair");
-    }
+    }*/
 
 }
+
+function isEven(nb) {
+    
+    return (nb%2 ==0);
+
+    /*let result = false;
+    if (nb % 2 == 0) {
+        result = true;
+    }
+    return result;*/
+}
+
+
+
 
 function exo2() {
 
-    var sentence = prompt("Phrase");
+    /*var sentence = prompt("Phrase");
 
     var space = sentence.split(" ");
 
-    alert(space[0]);
+    alert(space[0]);*/
+
+    let p = prompt("phrase");
+    alert(firstWord(p));
 
 }
 
+function firstWord(p){
+    console.log(p.split(' '));
+    p.split(' ')[0];
+    return p.split(' ')[0];
+}
+
+
+
+
 function exo3() {
 
-    var word = prompt("mot");
+    alert(firstLetter("hELLO world"))
+
+   /*var word = prompt("mot");
 
     word = word[0].toUpperCase() + word.substring(1).toLowerCase();
 
-    alert(word);
+    alert(word);*/
+}
+
+function firstLetter(p){
+   
+    return p.charAt(0).toUpperCase() + p.slice(1).toLowerCase();
 }
 
 function exo3bis() {
 
-    CamelCase = function CamelCase(str) {
-        return str.replace(/\W+(.)/g, function (match, chr) {
-            return chr.toUpperCase();
-        });
-    }
-
-    console.log(CamelCase("Java Script Exercises"));
+    alert(camelCase("HELLO world"))
 }
+
+function camelCase(p) {
+    let d = p.split(' ');
+    let strResult = d[0].toLowerCase();
+
+    for(let i = 1; i < d.length; i++) {
+        strResult += firstLetter(d[i]);
+    }
+    return strResult;
+}
+
+
+
+
 
 function exo4() {
     
@@ -53,7 +96,48 @@ function exo4() {
     }
 }
 
-function exo5() {
+function exo5(mdp) {
+    console.log(force('HelloWorld'))    
+}
 
+function force(mdp){
+    let l = mdp.length;
+    let n = 0;
+    let force = l * (Math.log2(n));
+    return force;
+}
 
+function alphabet(mdp){
+    n = 0;
+
+    if(mdp.match(/[0-9]/)){
+        n += 10;
+    }
+    if (mdp.match(/[a-z]/)){
+        n += 26;
+    }
+    if (mdp.match(/[A-Z]/)){
+        n += 26;
+    }
+
+}
+
+function exo6() {
+
+    var genre;
+    do{
+        genre = prompt("genre");
+        console.log('f')
+        genre = genre.toLowerCase();
+        genre = genre.replace('â', 'a')
+        
+    }while(genre != 'femelle' && genre != 'male'){
+        console.log('succes : ', genre);
+        console.log()
+    }
+}
+
+function exo9(){
+
+    
 }
